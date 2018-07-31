72838447b66f1ae6d520a89b5630a2af06a83455
Neha Sharma <ns3786@nyu.edu>
2018-07-31 15:39:22 -0400

    Minor changes

--- mySolution.sh
+++ mySolution.sh
@@ -4,7 +4,10 @@ pip3 install python-dateutil --upgrade
 pip3 install bs4
 echo Please wait while we display the current trends in the provided date range
 python webScraper.py "$1" "$2"
-for file in Result/*
-do
-	open "$file"
-done
+if [ -d "Result" ]
+then
+	for file in Result/*
+	do
+		open "$file"
+	done
+fi
